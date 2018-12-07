ec45d26ab6bf93a8a654ca1192c43cafabe9fe48
Jono <jonobarel@gmail.com>
2018-12-07 22:24:51 +0200

    transfer WIP to rpi (broken!)

---
+++ playback.py
@@ -0,0 +1,55 @@
+#This is a wrapper for interfacing with an instance of omxplayer.
+#When a song is played, the application launches an omxplayer process.
+#Aspiration: While the song is playing it's possible to pause and resume, control volume, through appropriate inputs.
+import subprocess
+
+class SongPlayer:
+
+    def __init__(self, files_list: [], path = "music/"):
+        self.current = 0
+        self.files_list = files_list
+        self.procs = []
+        self.current_proc = None
+
+        if files_list == None:
+            raise ValueError('missing parameter: files_list cannot be None')
+
+        if !isinstance(files_list, list):
+            raise TypeError('files_list parameter is not a list')
+
+        if len(files_list) < 1:
+            raise ValueError('files_list is empty')
+
+        for f in files_list:
+            if !isinstance(f, str):
+                raise TypeError('files_list must only contain strings')
+        
+    
+
+    #plays either the song at index i, or the next song on the list.
+    #If a song is already playing, it will first stop the playback and begin the next one.
+    def play(self):
+        #start a new track
+        if (self.current_proc == None || current_proc.poll() != None):
+            self.current_proc = Popen(["omxplayer", path+self.files_list[self.current]],start_new_session=True, stdin=subprocess.PIPE)
+            self.current+=1%len(files_list)
+            
+        #if a process exists, check if it is still running.
+        #If so, send a pause signal.
+
+        elif self.current_proc.poll() == None:
+            current_proc.communicate('p')
+        else: #process is not running
+            raise Exception('Could not pause or play track')
+            
+            
+                                  
+
+    def next(self):
+        
+
+    #Pause the currently playing song
+    def pause(self):
+
+    def stop(self):
+        
--- rpi_musicbox.py
+++ rpi_musicbox.py
@@ -1,21 +1,27 @@
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 import time
 import os
 import signal
 
 from subprocess import Popen as po
 
-path = '/home/pi/music/'
-files = os.listdir(path)
+music_path = 'music/' #relative path to music. Maybe make this configurable? let's keep it simple for now.
+wd = os.path.dirname(os.path.realpath(__file__)) #working directory
+
+files = os.listdir(music_path)
 for f in files:
     print(f)
-    #subprocess.run(["omxplayer", path+f])
+    #subprocess.run(["omxplayer", music_path+f])
     #time.sleep(3)
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(18,GPIO.OUT)
-GPIO.setup(25,GPIO.IN)
+NEXT=25
+PLAY=None
+LED=18
+
+#GPIO.setwarnings(False)
+#GPIO.setmode(GPIO.BCM)
+#GPIO.setup(LED,GPIO.OUT)
+#GPIO.setup(NEXT,GPIO.IN)
 
 #TODO get a list of running players and kill them all. KILL THEM WITH FI9E!
 #TODO set the input and output labels to variables so that people using this can decide where they want to make connections
@@ -28,23 +34,23 @@ n = len(files)
 #signal that the app is running
 #TODO set the sleep delay to a variable and make this better
 #what is the python equivalent of ruby's "3.times.do {}"?
-po(["omxplayer", "/home/pi/audio/mariocoin.mp3"])
+po(["omxplayer", "audio/341695__projectsu012__coins-1.wav"])
 for i in [1,2,3]:
-    GPIO.output(18,True)
+    GPIO.output(LED,True)
     time.sleep(0.2)
-    GPIO.output(18,False)
+    GPIO.output(LED,False)
     time.sleep(0.2)
     
 
 while True:
-    if GPIO.input(25):
-        GPIO.output(18, False)
+    if GPIO.input(NEXT):
+        GPIO.output(LED, False)
     else:
         if (i==n): #we're playing the last song. Time to stop and reset the player to 0
             if p!= None:
                 os.killpg(os.getpgid(p.pid),1)
             i=0
-        GPIO.output(18,True)
+        GPIO.output(LED,True)
         #create a process to play a song, and track it. We first need to check if there is one.
         if (p != None): #kill the currently playing song
             "stopping playback for "+p.args[1]
@@ -52,7 +58,7 @@ while True:
             #TODO add a LED for playback
             os.killpg(os.getpgid(p.pid), 1)
         print("playing "+files[i])
-        p = po(["omxplayer", path+files[i]],start_new_session=True)
+        p = po(["omxplayer", music_path+files[i]],start_new_session=True)
         #i = (i+1)%n #next file or go to first
         i+=1
         time.sleep(1)
